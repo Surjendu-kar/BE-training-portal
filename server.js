2df2c00334647c9ddefef21938c3ddba85af7134
Surjendu kar <rahulgotp9988@gmai.com>
2025-04-04 11:17:47 +0530

    refactor: update root API response message and remove Vercel configuration file

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use("/api/batches", batchRoutes);
 
 // Basic route
 app.get("/", (req, res) => {
-  res.status(200).send("API is running");
+  res.send("Backend is running!");
 });
 
 const PORT = process.env.PORT || 5000;
