4054dd0ef3dfa05ea5ef0c2469001d7e59ccd367
Surjendu kar <rahulgotp9988@gmai.com>
2025-04-07 19:29:40 +0530

    refactor: update CORS middleware to conditionally set origin based on environment

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://iqnaut-training-portal.web.app/"
+        : "http://localhost:3000",
     credentials: true,
   })
 );
